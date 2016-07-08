a6b7873b7ab21283c9844907c19d8bca97720d54
lorenzo <lorenzogamboagarcia@gmail.com>
2016-07-08 19:33:30 +0200

    Revert "Revert "Setting up front""
    This reverts commit 1cc148bcb23098e846e949aa9a524a46ce2ccdc2.

---
+++ server/creategame.js
@@ -0,0 +1,29 @@
+/**
+ *@Author: Lorenzo Gamboa Garcia
+ *@Description: Js file to create rooms
+ */
+ 
+var io;
+var gameSocket;
+
+
+
+/**
+ * This object will be instance when a user reach out create.html
+ */
+exports.initRoom = function(sio,socket){
+this.io = sio;
+this.gameSocket = socket;
+
+return console.log("Setting room");
+}
+
+function hostinRoom(){
+
+}
+
+function removingRoom(){
+	
+	
+}
+
