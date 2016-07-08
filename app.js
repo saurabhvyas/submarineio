e6c31023a12a0aaa3033535136224343c681a429
lorenzo <lorenzogamboagarcia@gmail.com>
2016-07-08 19:27:31 +0200

    Setting up front

--- app.js
+++ app.js
@@ -14,17 +14,29 @@ var path = require('path');
 //Creating Expres app
 var app = express();
 
+//Importing create class
+var create_game = require('./server/creategame');
+
 
 //Acessing static files (css,js..)
 app.use("/lib", express.static(path.join(__dirname + '/public/lib')));
 
 // Create a simple Express application
-app.get('/rankings.html', function(req, res){
+app.get('/rankings.*', function(req, res){
    //console.log("Página solicitada ===> "+req.params[0]);
   // Serve static html, js, css, and image files from the 'public' directory
   res.sendFile(__dirname + '/public/rankings.html');
 	
 });
+
+// Create a simple Express application
+app.get('/create.*', function(req, res){
+   //console.log("Página solicitada ===> "+req.params[0]);
+  // Serve static html, js, css, and image files from the 'public' directory
+  res.sendFile(__dirname + '/public/create.html');
+	
+});
+
 
 // Create a simple Express application
 app.get('/', function(req, res){
@@ -40,12 +52,15 @@ var server = require('http').createServer(app).listen(process.env.PORT || 8080);
 
 // Instantiate Socket.io and will be listeing to Express/HTTP server
 var io = require('socket.io').listen(server);
-console.log("Server launched on port 8080");
+console.log("Server launched on port 8080 or " + process.env.PORT);
 
 
-io.sockets.on('connection', function (socket) {
-	//console.log('client connected');
-	game.initGame(io,socket);
-	
-	
+io.on('connection', function(socket){
+ console.log('a user connected');
+  socket.on('message', function(msg){
+	  console.log(msg);
+   });
+ create_game.initRoom();
+
 });
+
