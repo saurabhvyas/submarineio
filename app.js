ba424e44ddfed5c0c50977b329b3aaa9c2425caf
lorenzo <lorenzogamboagarcia@gmail.com>
2016-07-12 03:35:01 +0200

    Make rooms :8ball:
    Http get request on making rooms

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
  
 // Import Express module
 var express = require('express');
+var url = require('url');
 
 //Import Expres path
 var path = require('path');
@@ -15,7 +16,7 @@ var path = require('path');
 var app = express();
 
 //Importing create class
-var create_game = require('./server/creategame');
+//var create_game = require('./server/creategame');
 
 //Numer of Users online
 var nUsers = 0;
@@ -25,7 +26,17 @@ var rooms=[];
 //Acessing static files (css,js..)
 app.use("/lib", express.static(path.join(__dirname + '/public/lib')));
 
-// Create a simple Express application
+//HTPP GET request to make room & start Game
+app.get('/game', function(req, res){
+	var url_parts = url.parse(req.url, true);
+	var roomSelected = url_parts.query;
+   console.log(roomSelected["rn"]);
+
+  res.sendFile(__dirname + '/public/game.html');
+	
+});
+
+// This is ranking of top 100 players
 app.get('/rankings.*', function(req, res){
    //console.log("Página solicitada ===> "+req.params[0]);
   // Serve static html, js, css, and image files from the 'public' directory
@@ -33,7 +44,7 @@ app.get('/rankings.*', function(req, res){
 	
 });
 
-// Create a simple Express application
+// This is page to create rooms
 app.get('/create.*', function(req, res){
    //console.log("Página solicitada ===> "+req.params[0]);
   // Serve static html, js, css, and image files from the 'public' directory
@@ -41,9 +52,9 @@ app.get('/create.*', function(req, res){
 	
 });
 
-// Create a simple Express application
+// This is rooms available page
 app.get('/join.*', function(req, res){
-   //console.log("Página solicitada ===> "+req.params[0]);
+  //console.log("Página solicitada ===> "+req.params[0]);
   // Serve static html, js, css, and image files from the 'public' directory
   res.sendFile(__dirname + '/public/join.html');
 	
@@ -51,7 +62,7 @@ app.get('/join.*', function(req, res){
 
 
 
-// Create a simple Express application
+// This is index.html
 app.get('/', function(req, res){
    console.log("Pagina principal solicitada");
    res.sendFile(__dirname + '/public/index.html');
@@ -88,7 +99,11 @@ io.on('connection', function(socket){
 	console.log(rooms[x]);
 
 	}
+	
+	socket.emit('createdcheck', "Created");
   });
+  
+
   
    socket.emit('rooms', rooms, function (data) {
       
